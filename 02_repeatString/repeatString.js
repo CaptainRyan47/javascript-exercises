4f910bc2d922de97f2db83a3c1e27ed9b542117d
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-04-20 16:24:24 -0700

    Finish 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let rString = '';
+  if (num < 0) return 'ERROR';
+  for (let i = 0; i < num; i++) {
+    rString = rString + string;
+  };
+  return rString;
 };
 
 // Do not edit below this line
