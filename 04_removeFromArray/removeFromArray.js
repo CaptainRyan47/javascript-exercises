9a963d23e8ba591d22433a76e1259e74c73c4d22
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-04-24 12:55:54 -0700

    Finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  const arrayLength = array.length;
+  const argsLength = args.length;
+  for (let i = 0; i < argsLength; i++) {
+    for (let f = arrayLength - 1; f >= 0; f--) {
+      if (array[f] === args[i]) array.splice(f, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
