a97493d9d24146c27f23f6381a64d0a25ea204bc
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-04-24 17:11:38 -0700

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  let bigNum = 0;
+  let littleNum = 0;
+  if (!(typeof a === 'number') || !(typeof b === 'number')) return 'ERROR';
+  if (a < 0 || b < 0) return 'ERROR';
+  if (a > b) {
+    bigNum = a;
+    littleNum = b;
+  } else {
+    bigNum = b;
+    littleNum = a;
+  }
+  for (i = 0; i <= bigNum - littleNum; i++) {
+    sum += littleNum + i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
