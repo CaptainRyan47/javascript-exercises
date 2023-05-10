f0c70fb7f063ae6629476eee7e8854d4a2219502
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-05-10 13:15:45 -0700

    Finish 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  array.forEach(num => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let product = array[0];
+  array.forEach((num, index) => {
+    if (index === 0) return;
+    product *= num;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  if (b === 0) return 1;
+  let c = a;
+  for (let i = 1; i < b; i++) {
+    c *= a;
+  }
+  return c;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let b = 1;
+  for (let i = 2; i <= a; i++) b *= i ;
+  return b;
 };
 
 // Do not edit below this line
