79b437dd3bc1f4e70ea6cb43b1aacacc869c8899
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-05-10 16:45:53 -0700

    Finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+  if (a < 0) return 'OOPS';
+  let fibSeq = [1,1];
+  for (let i = 2; i < a; i++) {
+    fibSeq[i] = fibSeq[i-1] + fibSeq[i-2];
+  }
+  return fibSeq[a-1];
 };
 
 // Do not edit below this line
