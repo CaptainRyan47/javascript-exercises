0be5158dd77a83c690631ffecbb740760e93b137
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-05-11 15:18:15 -0700

    Finish 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function (array) {
+  let titles = [];
+  let i = 0;
+  array.forEach(item => {
+    titles[i++] = item['title'];
+  });
+  return titles;
 
+  //⬇️ Better way to do this ⬇️
+  //return (array.map(item => item.title));
 };
 
 // Do not edit below this line
