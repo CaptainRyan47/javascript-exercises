cf56ae6f3ad9c2ac05e87a74647d2ef68d2fb988
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-05-11 17:40:15 -0700

    Finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const year = new Date().getFullYear();
+  let oldest;
+  let oldestAge = 0;
+  let age = 0;
+  people.forEach(person => {
+    'yearOfDeath' in person ?
+      age = person.yearOfDeath - person.yearOfBirth
+      : age = year - person.yearOfBirth;
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldest = person;
+    }
+  });
+  return oldest;
 };
 
 // Do not edit below this line
